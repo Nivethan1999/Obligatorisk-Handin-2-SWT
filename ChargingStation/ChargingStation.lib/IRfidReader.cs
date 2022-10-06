576bbdd3c70f4a9b82fa3bf1b1d2c2aeffcbaef2
AlanKhamo <alankhamo@gmail.com>
2022-10-06 18:48:44 +0200

    add RfidReader and IRfidReader

---
+++ ChargingStation/ChargingStation.lib/IRfidReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingStation.lib
+{
+     public interface IRfidReader
+     {
+          public void RfidDetected(int Id);
+
+          public void IsConnected();
+
+          public void CheckId(int oldid, int id);
+
+     }
+}
