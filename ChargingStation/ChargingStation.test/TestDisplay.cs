93483a16e813c60ce2bdabca219e04ef7e353fcc
AlanKhamo <alankhamo@gmail.com>
2022-10-13 12:02:59 +0200

    Test ConnectionError and LoadRFID for Display

--- ChargingStation/ChargingStation.test/TestDisplay.cs
+++ ChargingStation/ChargingStation.test/TestDisplay.cs
@@ -27,5 +27,19 @@ namespace ChargingStation.test
              display_.ConnectPhone();
              Assert.That(output.ToString(), Contains.Substring("Tilslut telefon"));
         }
-    }
+
+        [Test]
+        public void ConnectionError()
+        {
+             display_.ConnectionError();
+             Assert.That(output.ToString(), Contains.Substring("Din telefon er ikke ordentlig tilsluttet. Prøv igen."));
+        }
+        [Test]
+        public void LoadRFID()
+        {
+             display_.LoadRFID();
+             Assert.That(output.ToString(), Contains.Substring("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op."));
+        }
+
+     }
 }
